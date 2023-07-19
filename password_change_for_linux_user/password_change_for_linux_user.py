1ed81737f9723b78165759127fba6cb78b90da93
prykhodchenko <po587747@gmail.com>
2023-07-19 16:33:27 +0300

    Added oop password generator

--- password_change_for_linux_user/password_change_for_linux_user.py
+++ password_change_for_linux_user/password_change_for_linux_user.py
@@ -6,77 +6,99 @@ from colorama import Fore, Style
 
 from user_data_manipulator_on_linux import UserDataManipulatorOnLinux
 from password_generator import PasswordGenerator
+from password_validator import PasswordValidator
 
 
 # The initial method of password change
 def change_linux_user_password(username: str) -> None:
-  typed_password: str = ask_user_to_type_password()
+    typed_password: str = ask_user_to_type_password()
 
-  if typed_password:
-    set_typed_password(username, typed_password)
-  else:
-    set_auto_generated_password(username)
+    if typed_password:
+        set_typed_password(username, typed_password)
+    else:
+        set_auto_generated_password(username)
 
 
-# Setting auto-generated password
+# Method change existing user password on auto-generated password
 def set_auto_generated_password(username: str) -> None:
-  #ask the user to print the password length.
-  password_length: int = PasswordGenerator.ask_password_length()
+    # ask the user to print the password length.
+    password_length: int = ask_password_length()
+    password_generator = PasswordGenerator(password_length, True, True, True, True)
 
-  # generate a strong password
-  new_password: str = PasswordGenerator.generate_password(password_length)
+    # generate a strong password
+    new_password: str = password_generator.password
+    password_validator = PasswordValidator(new_password)
 
-  #check password requirements
-  password_requirements_results: Dict = PasswordGenerator.check_password_requirements(new_password)
+    # check password requirements
+    password_validation_results: dict = password_validator.password_validation_result
 
-  # After we are rewriting the user password and print result
-  UserDataManipulatorOnLinux.set_user_password(username, new_password)
-  print_result_of_set_user_password(username, new_password, password_requirements_results)
+    # After we are rewriting the user password and print result
+    UserDataManipulatorOnLinux.set_user_password(username, new_password)
 
-# Setting typed password
+    print_result_of_set_user_password(username, new_password, password_validation_results)
+
+
+# Method change existing user password on typed password
 def set_typed_password(username: str, typed_password: str) -> None:
-  #check password requirements
-  password_requirements_results: Dict = PasswordGenerator.check_password_requirements(typed_password)
+    # check password requirements
+    password_validator = PasswordValidator(typed_password)
+    password_validation_results: dict = password_validator.password_validation_result
+    is_password_valid: bool = password_validator.is_password_valid
 
-  if all(password_requirements_results.values()):
-    UserDataManipulatorOnLinux.set_user_password(username, typed_password)
-    print_result_of_set_user_password(username, typed_password, password_requirements_results)
-  else:
-    print(password_requirements_results)
-    print(Fore.RED + '\nYour password is not strong enough' + Style.RESET_ALL)
-    print_password_requirements_results(password_requirements_results)
-    change_linux_user_password(username)
+    if is_password_valid:
+        UserDataManipulatorOnLinux.set_user_password(username, typed_password)
+        print_result_of_set_user_password(username, typed_password, password_validation_results)
+    else:
+        print(Fore.RED + '\nYour password is not strong enough' + Style.RESET_ALL)
+        print_password_validation_results(password_validation_results)
+        change_linux_user_password(username)
 
 
+# Method ask the user to type a username
+# It needs to exist Linux user
 def ask_username() -> str:
-  entered_username: str = input('Enter a username: ')
+    entered_username: str = input('Enter a username: ')
 
-  if UserDataManipulatorOnLinux.check_if_user_exists(entered_username):
-    return entered_username
-  else:
-    ask_username()
+    if UserDataManipulatorOnLinux.check_if_user_exists(entered_username):
+        return entered_username
+    else:
+        ask_username()
 
 
+# Method ask the user to type the new password
 def ask_user_to_type_password() -> str:
-  return getpass.getpass('Enter a password or press Enter to generate a new one: ')
+    return getpass.getpass('Enter a password or press Enter to generate a new one: ')
 
 
-def print_result_of_set_user_password(username: str, password: str, password_requirements_results: Dict) -> None:
-  print(f'\nUsername: {username}')
-  print(f'Password: {password}')
+# Method ask the user to type the password length
+# It needs to be at least 8 characters
+def ask_password_length() -> int:
+    password_length: int = int(input('Please enter the desired password length: '))
 
-  if password_requirements_results:
-    print_password_requirements_results(password_requirements_results)
+    if password_length < 8:
+        while password_length < 8:
+            print('Your password length must be at least 8 characters!')
+            password_length = int(input('Please enter the desired password length: '))
+
+    return password_length
 
 
-def print_password_requirements_results(password_requirements_results: Dict) -> None:
-  for index, password_requirements_result in enumerate(password_requirements_results):
-    print(f"\t{index + 1}. {password_requirements_result.replace('_', ' ').title()}: "
-          f'{Fore.GREEN if password_requirements_results[password_requirements_result] else Fore.RED}'
-          f"{'✓' if password_requirements_results[password_requirements_result] else 'X'}" + Style.RESET_ALL)
+def print_result_of_set_user_password(username: str, password: str, password_validation_results: Dict) -> None:
+    print(f'\nUsername: {username}'
+          f'\nPassword: {password}')
+
+    if password_validation_results:
+        print_password_validation_results(password_validation_results)
+
+
+def print_password_validation_results(password_validation_results: Dict) -> None:
+    for index, password_validation_result in enumerate(password_validation_results):
+        print(f"\t{index + 1}. {password_validation_result.replace('_', ' ').title()}: "
+              f'{Fore.GREEN if password_validation_results[password_validation_result] else Fore.RED}'
+              f"{'✓' if password_validation_results[password_validation_result] else 'X'}" + Style.RESET_ALL)
 
 
 if __name__ == '__main__':
-  username: str = ask_username()
+    username: str = ask_username()
 
-  change_linux_user_password(username)
+    change_linux_user_password(username)
--- password_change_for_linux_user/password_generator.py
+++ password_change_for_linux_user/password_generator.py
@@ -3,60 +3,38 @@
 import string
 import random
 import numpy as np
-from typing import List, Dict
+from typing import List
 
 
 class PasswordGenerator:
-  uppercase_letters: str = string.ascii_uppercase
-  lowercase_letters: str = string.ascii_lowercase
-  digits: str = string.digits
-  ascii_characters: str = string.punctuation
-  all_characters: str = digits + lowercase_letters + ascii_characters + uppercase_letters
+  __password_length: int = 8
+  __uppercase_letters: bool = False
+  __lowercase_letters: bool = False
+  __digits: bool = False
+  __punctuation: bool = False
+  __password: str or None = None
 
-  @staticmethod
-  def check_password_requirements(password: str) -> Dict:
-    password_requirements_results: Dict = {
-      'password_length': len(password) >= 8,
-      'punctuation': False,
-      'upper_case_char': False,
-      'lower_case_char': False,
-      'digit': False
-    }
+  def __init__(self, password_length, uppercase_letters, lowercase_letters, digits, punctuation):
+    self.__password_length: int = password_length
+    self.__uppercase_letters: bool = uppercase_letters
+    self.__lowercase_letters: bool = lowercase_letters
+    self.__digits: bool = digits
+    self.__punctuation: bool = punctuation
 
-    for i in password:
-      if i in string.punctuation:
-        password_requirements_results['punctuation'] = True
-      elif i.isupper():
-        password_requirements_results['upper_case_char'] = True
-      elif i.islower():
-        password_requirements_results['lower_case_char'] = True
-      elif i.isdigit():
-        password_requirements_results['digit'] = True
-
-    return password_requirements_results
-
-  @staticmethod
-  def ask_password_length() -> int:
-    password_length: int = int(input('Please enter the desired password length: '))
-
-    if password_length < 8:
-      while password_length < 8:
-        print('Your password length must be at least 8 characters!')
-        password_length = int(input('Please enter the desired password length: '))
-
-    return password_length
-
-  @classmethod
-  def generate_password(cls, password_length: int) -> str:
+    all_characters: str = string.digits + string.punctuation + string.ascii_lowercase + string.ascii_uppercase
     required_characters: List[str] = PasswordGenerator.get_required_characters()
-    password_length_without_required_characters: int = password_length - len(required_characters)
-    random_chose_characters: List[str] = random.choices(cls.all_characters, weights=None,
+    password_length_without_required_characters: int = self.__password_length - len(required_characters)
+    random_chose_characters: List[str] = random.choices(all_characters, weights=None,
                                                         k=password_length_without_required_characters)
-    password_list: List[str] = np.concatenate((random_chose_characters, required_characters))
+    password_list: List = list(np.concatenate((random_chose_characters, required_characters)))
 
     random.shuffle(password_list)
 
-    return PasswordGenerator.list_to_string(password_list)
+    self.__password = self.list_to_string(password_list)
+
+  @property
+  def password(self):
+    return self.__password
 
   @staticmethod
   def list_to_string(list: List[str]) -> str:
@@ -67,11 +45,15 @@ class PasswordGenerator:
 
     return string
 
-  @classmethod
-  def get_required_characters(cls) -> List[str]:
+  @staticmethod
+  def get_required_characters() -> List[str]:
+    print(random.choices(string.ascii_uppercase, weights=None, k=1)[0],
+          random.choices(string.ascii_lowercase, weights=None, k=1)[0],
+          random.choices(string.punctuation, weights=None, k=1)[0],
+          random.choices(string.digits, weights=None, k=1)[0], )
     return [
-      random.choices(cls.uppercase_letters, weights=None, k=1)[0],
-      random.choices(cls.lowercase_letters, weights=None, k=1)[0],
-      random.choices(cls.ascii_characters, weights=None, k=1)[0],
-      random.choices(cls.digits, weights=None, k=1)[0],
+      random.choices(string.ascii_uppercase, weights=None, k=1)[0],
+      random.choices(string.ascii_lowercase, weights=None, k=1)[0],
+      random.choices(string.punctuation, weights=None, k=1)[0],
+      random.choices(string.digits, weights=None, k=1)[0],
     ]
